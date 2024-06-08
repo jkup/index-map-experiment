4874af1233d28d536d555225152dd34d57cdfccf
Jon Kuperman <jon.kuperman@gmail.com>
2024-06-08 17:50:34 +0100

    configure babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    sourceMapFilename: "bundle.js.map",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            sourceMaps: true,
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+};
