3fc4c027a762e8844b4f5690d96fc53727558356
Jon Kuperman <jon.kuperman@gmail.com>
2024-06-08 17:52:05 +0100

    basic file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development", // Set to 'development' or 'production'
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
